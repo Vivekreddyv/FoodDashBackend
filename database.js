a957c695da853b015e9d3554d6bfd679a1ec8be4
Vivekreddyv <127015119+Vivekreddyv@users.noreply.github.com>
2024-06-21 13:00:14 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const mongodbURI='mongodb+srv://saivivek1414:FoodDash8797@cluster0.gbjbtgy.mongodb.net/FoodDashmern?retryWrites=true&w=majority'
+const mongodbURI=process.env.mongodbURI
 
 const mongodb=async()=>{
     await mongoose.connect(mongodbURI)
